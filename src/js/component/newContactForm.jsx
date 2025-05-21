9aa3cb26609feb6478d3965d1f0ccef3dc377cc4
Joan Simonutti <joansimonutticode@gmail.com>
2025-05-21 15:04:01 +0200

    fix error en pagina de inicio

--- src/js/component/newContactForm.jsx
+++ src/js/component/newContactForm.jsx
@@ -33,6 +33,7 @@ export const NewContactForm = () => {
         e.preventDefault();
         console.log(formData)
         actions.createContact(formData)
+        navigate('/');
     }
 
     return (
